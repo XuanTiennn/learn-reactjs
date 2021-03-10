fb2d1444b5586c0311b856e0f08bbeb042be1c5a
xuantien <taxuantien312000@gmail.com>
2021-03-10 20:41:06 +0700

    abc

---
+++ src/Demo.js
@@ -0,0 +1,9 @@
+function Demo() {
+  return (
+    <div>
+        <h1>abc</h1>
+        <p>safinfiwniafn</p>
+    </div>
+  );
+}
+export default Demo;
---
+++ src/component/Products.js
@@ -0,0 +1,8 @@
+import React from 'react';
+class Products extends React.Component {
+  render() {
+    return <div>{this.props.children}</div> 
+  }
+}
+
+export default Products;
